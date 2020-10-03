2762307cf48607c754fa4f6a2d35e7d083fe1a5e
Fellsmarch <harrisoncook4@gmail.com>
2020-10-03 22:27:13 +1300

    Update save_to_file to also create files if they don't exist. Update csv_string_to_html_table to take an optional table_id

--- harrison/html/csv_string_to_html_table.py
+++ harrison/html/csv_string_to_html_table.py
@@ -2,7 +2,7 @@ from .dict_to_html_table import dict_to_html_table
 from ..csv.create_csv_dict import create_csv_dict
 
 
-def csv_string_to_html_table(csv_string: str) -> str:
+def csv_string_to_html_table(csv_string: str, table_id: str="tableId") -> str:
     """
     Formats a CSV file represented as a string to an HTML table
     represented as a string.
@@ -10,4 +10,4 @@ def csv_string_to_html_table(csv_string: str) -> str:
     csv_list = csv_string.split("\r\n")
     headers = csv_list[0].split(",")
     csv_dict = create_csv_dict(headers, csv_list[1:])
-    return dict_to_html_table(csv_dict)
+    return dict_to_html_table(csv_dict, table_id)
